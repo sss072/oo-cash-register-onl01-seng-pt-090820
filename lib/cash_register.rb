87a48e14c65c2abc6ebf373418e20f870590872c
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-10-05 22:36:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     previous_total = total 
     @total += price * quantity 
-    previous_total 
+    self.last_transaction = amount * quantity 
   end
   def apply_discount
     if discount != 0 
